2013182c77b4c0b9363003a63db05f7c2d43d816
micheal okhaimoh <aokhaimoh@yahoo.com>
2025-02-13 17:14:35 +0100

    chore: added comment

--- lib/components/navigator_function.dart
+++ lib/components/navigator_function.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// PUSH BETWEEN SCREENS
 void pushScreen(BuildContext context, Widget screen) {
   Navigator.of(context).push(
     MaterialPageRoute<void>(
