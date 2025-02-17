0dc3704f8640299166eba5e58cf3775238db3ef0
micheal okhaimoh <aokhaimoh@yahoo.com>
2025-02-17 16:36:58 +0100

    feat: auth login and signup

--- lib/screens/auth_screens/register_screen.dart
+++ lib/screens/auth_screens/register_screen.dart
@@ -1,5 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:anutio_chat_app/components/input_decorations.dart';
+import 'package:anutio_chat_app/components/navigator_function.dart';
 import 'package:anutio_chat_app/components/rounded_button.dart';
+import 'package:anutio_chat_app/screens/main_screens/chat_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -11,10 +17,42 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   late String email;
   late String password;
+  bool isPasswordVisible = false;
 
-  bool isLoading = false;
+  bool showSpinner = false;
+
+  void setBusy(bool isLoading) {
+    setState(() {
+      showSpinner = isLoading;
+    });
+  }
+
+  Future<void> registerUser() async {
+    setBusy(true);
+
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      setBusy(false);
+
+      pushScreen(context, ChatPage());
+    } catch (e) {
+      setBusy(false);
+      if (kDebugMode) {
+        print(e);
+      }
+
+      final snackBar = SnackBar(content: Text(e.toString()));
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +62,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
         backgroundColor: Colors.white,
       ),
       body: ModalProgressHUD(
-        inAsyncCall: isLoading,
+        inAsyncCall: showSpinner,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: SingleChildScrollView(
@@ -57,18 +95,46 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
                 TextField(
                   textAlign: TextAlign.center,
-                  obscureText: true,
+                  obscureText: isPasswordVisible,
                   onChanged: (value) {
                     password = value;
                   },
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'Enter your Password'),
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Password',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        isPasswordVisible
+                            ? Icons.remove_red_eye
+                            : Icons.remove_red_eye_outlined,
+                      ),
+                    ),
+                  ),
                 ),
                 SizedBox(
                   height: 25,
                 ),
                 RoundedButton(
-                  onPressed: () {},
+                  onPressed: registerUser,
                   myText: 'Register',
                   myColor: Colors.blueAccent,
                 )
---
+++ lib/screens/main_screens/chat_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+        title: const Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
